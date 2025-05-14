6f629db8d91d596b855862d7066415154f929579
kaya0s <erwin.lanzaderas@gmail.com>
2025-05-15 00:20:03 +0800

    added google auth2

--- index.php
+++ index.php
@@ -42,28 +42,46 @@ session_start();
                         </div>
                             
 
-                            <button class="login-btn" type="submit" value="login"  name="login" >Log in</button>
-                            <a href="auth/googleAuth/google-callback.php?code=2" class="google-btn "> 
-                                <svg class="google-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48">
-                                <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
-                                <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/>
-                                <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/>
-                                <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/>
-                                <path fill="none" d="M0 0h48v48H0z"/>
-                                </svg>
-                                Sign in with Google
-                            </a>
-                        
-                            <p style="font-size:smaller" class="errormsg"> <?php
-                            
-                            //  require_once('auth/validateLogin.php');
-                            if(isset($_SESSION['loginmsg']))
-                             echo htmlspecialchars($_SESSION['loginmsg']);unset($_SESSION['loginmsg']); ?>
-                             </p>
-                             
-                            <p><a href="auth/forgotpassword/forgot-password.php">Forgot Password?</a></p>
+                        <?php
+                    require_once('vendor/autoload.php');
+                    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/');
+                    $dotenv->load();
 
-                        <p>Don't have an account? <a href="auth/register.php">Sign up</a></p>
+                    // Create Google Client
+                    $client = new Google_Client();
+                    $client->setClientId($_ENV['GOOGLE_CLIENT_ID']);
+                    $client->setClientSecret($_ENV['GOOGLE_CLIENT_SECRET']);
+                    $client->setRedirectUri($_ENV['GOOGLE_REDIRECT']);
+                    $client->addScope('email');
+                    $client->addScope('profile');
+
+                    // Generate the Google OAuth authorization URL
+                    $authUrl = $client->createAuthUrl();
+                    ?>
+
+                    <!-- Update the Google Sign-In button to use the generated auth URL -->
+                    <button class="login-btn" type="submit" value="login" name="login">Log in</button>
+                    <a href="<?php echo $authUrl; ?>" class="google-btn">
+                        <svg class="google-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48">
+                            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
+                            <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/>
+                            <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/>
+                            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/>
+                            <path fill="none" d="M0 0h48v48H0z"/>
+                        </svg>
+                        Sign in with Google
+                    </a>
+                                            
+                        <p style="font-size:smaller" class="errormsg"> <?php
+                        
+                        //  require_once('auth/validateLogin.php');
+                        if(isset($_SESSION['loginmsg']))
+                            echo htmlspecialchars($_SESSION['loginmsg']);unset($_SESSION['loginmsg']); ?>
+                            </p>
+                            
+                        <p><a href="auth/forgotpassword/forgot-password.php">Forgot Password?</a></p>
+
+                    <p>Don't have an account? <a style="color:rgb(5, 61, 151); margin-left: 6px;" href="auth/register.php">Sign up</a></p>
                         
                        
                     </form>
